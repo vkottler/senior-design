e60d66b4bccf515e7ab95babd34aa52adc3fabc3
vkottler <vkottler@wisc.edu>
2018-08-12 14:26:18 -0500

    Add some KiCad structure and a script to easily open projects.

--- cad/pcb/open.sh
+++ cad/pcb/open.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-kicad main.pro &
+# check usage
+if [ $# -ne 1 ]; then
+	echo "usage: (flight|ground)/(project_name)" && echo ""
+	echo "options" && echo "" 
+	echo "flight/" && ls flight && echo ""
+	echo "ground/" && ls ground && echo ""
+	exit 1
+fi
+
+# check if the input can be parsed
+if ! echo $1 | grep '/' >/dev/null; then
+	echo "Malformed input (no '/')."
+	exit 1
+fi
+
+ROOT=$(echo $1 | sed -r 's/\/(.*)//')
+PROJECT=$(echo $1 | sed -r 's/^(flight|ground)\///')
+PROJECT_FILE="$ROOT/$PROJECT/$PROJECT.pro"
+
+if [ ! -f $PROJECT_FILE ]; then
+	echo "Can't find project '$PROJECT_FILE'."
+	exit 1
+fi
+
+kicad $PROJECT_FILE &
