20047256be8cf5fe21ea75b4b3e2b43d6483cf2b
vkottler <vaughnkottler@gmail.com>
2018-10-12 16:52:48 -0500

    client-server pair seem to be working!

--- src/sim/classes/telemetry/Channel.py
+++ src/sim/classes/telemetry/Channel.py
@@ -6,39 +6,72 @@ Fault-Tolerant Quadcopter - telemetry channel
 # internal
 import json
 
+class ChannelException(Exception):
+    """ Custom exception for Channel-related events. """
+    pass
+
 class Channel(object):
-    """ """
+    """
+    Object for encapsulating data that can be transported as telemetry.
+    """
 
     @staticmethod
-    def channel_from_json(json_data):
-        """ """
-        pass
+    def from_json(json_data, telemetry_client):
+        """ Create a channel from a JSON String. """
 
-    def __init__(self, name, value_type, unit, telemetry_client):
-        """ """
+        json_dict = json.loads(json_data)
+
+        # convert type string back into the actual type
+        json_type = int
+        if json_dict["type"] == "float":
+            json_type = float
+        elif json_dict["type"] == "str":
+            json_type = str
+
+        return Channel(json_dict["name"], json_type,
+                       json_dict["unit"], telemetry_client,
+                       json_dict["value"])
+
+    def __init__(self, name, value_type, unit, telemetry_client,
+                 value=None):
+        """ Initialize a channel. """
 
         self.dict = {}
         self.dict["name"]  = name
-        self.dict["type"]  = str(value_type)
+        self.dict["type"]  = value_type.__name__
         self.dict["unit"]  = unit
-        self.dict["value"] = None
+        self.dict["value"] = value
         self.type = value_type
         self.telemetry_client = telemetry_client
 
+    def equals(self, other_channel, check_value=False):
+        """ Check if two channel objects are equal. """
+
+        if self.dict["name"] != other_channel.dict["name"]:
+            return False
+        elif self.dict["type"] != other_channel.dict["type"]:
+            return False
+        elif self.dict["unit"] != other_channel.dict["unit"]:
+            return False
+        if check_value and self.dict["value"] != other_channel.dict["value"]:
+            return False
+        return True
+
     def set_data(self, data):
-        """ """
+        """ Set a new value for this channel. """
 
         self.dict["value"] = data
 
-    def to_json(self, pretty=False):
-        """ """
+    def to_json(self):
+        """ Convert this channel to JSON. """
 
-        return json.dumps(self.dict, ensure_ascii=True)
-
-    def to_byte():
-        """ """
-        pass
+        return json.dumps(self.dict, sort_keys=True)
 
     def publish(self):
-        """ """
-        pass
+        """
+        Publish this channel's data as telemetry via its telemetry client.
+        """
+
+        if not self.telemetry_client.started or self.telemetry_client.stopped:
+            raise ChannelException("Telemetry client not active.")
+        self.telemetry_client.data.put(self)
--- src/sim/classes/telemetry/Client.py
+++ src/sim/classes/telemetry/Client.py
@@ -4,37 +4,61 @@ Fault-Tolerant Quadcopter - telemetry client
 """
 
 # built-in
+import logging
 import queue
 import socket
 import threading
 
 class TelemetryClient(object):
-    """ """
+    """
+    A TCP-messaging based Telemetry Client that publishes data to a 
+    telemetry server.
+    """
 
-    def __init__(self, port=10018):
-        """ """
+    log = logging.getLogger(__name__)
+
+    def __init__(self, port=10018, name="Generic Client"):
+        """ Initialize the Telemetry Client. """
 
         self.port = port
+        self.name = name
         self.sock = socket.socket()
         self.data = queue.Queue()
         self.thread = threading.Thread(target=self.stream_telemetry)
+        self.started = False
+        self.stopped = False
 
     def stream_telemetry(self):
-        """ """
+        """
+        Write any requested Channel objects as JSON to the Telemetry Server
+        as they appear in the queue.
+        """
 
-        while True:
-            channel_data = self.data.get()
-            # TODO, figure out JSON conversion
+        while not self.stopped:
+            try:
+                channel_obj = self.data.get_nowait()
+                self.sock.sendall(bytearray(channel_obj.to_json() + "\n", encoding="utf-8"))
+                self.data.task_done()
+            except queue.Empty:
+                pass
 
     def start(self):
-        """ """
+        """
+        Attempt to connect to a Telemetry Server and run the telemetry
+        service in another thread.
+        """
 
-        self.sock.create_connection(("127.0.0.1", self.port))
+        self.sock.connect(("127.0.0.1", self.port))
         self.thread.start()
-        # TODO, log?
+        self.started = True
+        TelemetryClient.log.info("'%s' was started.", self.name)
 
     def stop(self):
-        """ """
+        """
+        Disconnect from the Telemetry Server and stop the telemetry service.
+        """
 
-        self.sock.close()
+        self.stopped = True
         self.thread.join()
+        self.sock.close()
+        TelemetryClient.log.info("'%s' was stopped.", self.name)
