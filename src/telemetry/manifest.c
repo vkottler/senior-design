b09f20c96307cb3972a9577331b015b87fdd12a9
vkottler <vkottler@wisc.edu>
2018-10-19 02:29:52 -0500

    working telemetry packet generation, need to test transport over network interface now

--- src/telemetry/manifest.c
+++ src/telemetry/manifest.c
@@ -4,15 +4,14 @@
 
 #include "telemetry.h"
 
-#include <stdio.h>
 #include <stdlib.h>
 
-channel_manifest_t *create_channel_manifest(unsigned int capacity)
+channel_manifest_t *channel_manifest_create(uint32_t capacity)
 {
     channel_manifest_t *manifest = malloc(sizeof(channel_manifest_t));
     if (!manifest)
     {
-        telemetry_debug("%s: malloc failed\n", __func__);
+        telemetry_debug("%s: malloc failed\r\n", __func__);
         return NULL;
     }
     manifest->count = 0;
@@ -20,9 +19,19 @@ channel_manifest_t *create_channel_manifest(unsigned int capacity)
     if (!manifest->channels)
     {
         free(manifest);
-        telemetry_debug("%s: calloc failed\n", __func__);
+        telemetry_debug("%s: calloc failed\r\n", __func__);
         return NULL;
     }
     manifest->capacity = capacity;
     return manifest;
+}
+
+void channel_manifest_print(FILE *stream, channel_manifest_t *manifest)
+{
+    fputs("********************\r\n", stream);
+    fprintf(stream, "Count:    %u\r\n", manifest->count);
+    fprintf(stream, "Capacity: %u\r\n", manifest->capacity);
+    for (int i = 0; i < manifest->count; i++)
+        channel_print(stream, &manifest->channels[i]);
+    fputs("********************\r\n", stream);
 }
