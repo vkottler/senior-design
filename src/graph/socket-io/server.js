0869417c2b655a60af5874f736cf85ac76a28916
katwalmayank <Nightmarewr360@gmail.com>
2018-11-11 23:42:04 -0600

    TCP supported

---
+++ src/graph/socket-io/server.js
@@ -0,0 +1,54 @@
+let express = require('express')
+var app = express();
+var http = require('http').Server(app);
+
+//TCP SHIT
+var net = require('net');
+var HOST = 'localhost';
+var PORT = 5000;
+
+app.use(express.static("./JS"))
+
+//port to receive data 
+var io_3000 = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io_3000.on('connection', (socket) => {
+  console.log('A user is connected to receive data -- id = ' + socket.id);
+  socket.on('disconnect', function(){
+    console.log('A user is disconnected to receive data -- id = ' + socket.id);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});   
+
+//TCP CONNECTION 
+var server = net.createServer();
+server.listen(PORT, HOST);
+console.log('listening on port: '+ PORT)
+
+server.on('connection', function(sock) {
+    console.log('CONNECTED to TCP');
+    sock.on('data', function(data) {
+      for (const b of data) {
+        console.log(b);
+      }
+      var incoming_data = []
+      incoming_data.push({
+        x: data[0], 
+        y: data[1] 
+      });
+      io_3000.emit('dataLine', incoming_data); 
+    }); 
+
+});
+server.on('disconnect', function() {
+  console.log('TCP connection closed');
+});
+
