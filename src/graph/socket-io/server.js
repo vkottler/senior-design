e85691601e6c432d0c788cf82115b9220c91f59d
Darth Vader <you@example.com>
2018-12-04 09:15:45 +0000

    fixed manifest parsing

--- src/graph/socket-io/server.js
+++ src/graph/socket-io/server.js
@@ -12,16 +12,16 @@ const batt_data_line = "batt_line"
 
 const buffer_offset = 4;
 
-//TESTING UI LOOK, REMOVE 
+//TESTING UI LOOK, REMOVE
 function getRandomIntInclusive(min, max)
 {
     min = Math.ceil(min);
     max = Math.floor(max);
-    // The maximum is inclusive and the minimum is inclusive 
+    // The maximum is inclusive and the minimum is inclusive
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//port to receive data 
+//port to receive data
 var io_3000 = require('socket.io')(http);
 app.use(express.static("./"));
 app.get('/', function(req, res) {
@@ -30,30 +30,30 @@ app.get('/', function(req, res) {
 
 io_3000.on('connection', (socket) => {
     console.log('A user is connected to receive data -- id = ' + socket.id);
-    var server_status = 1 
-    io_3000.emit("server_status", server_status); 		
+    var server_status = 1;
+    io_3000.emit("server_status", server_status);
     socket.on('disconnect', function() {
         server_status = 0;
         console.log('A user is disconnected to receive data -- id = ' + socket.id);
-        io_3000.emit("server_status", server_status); 		
+        io_3000.emit("server_status", server_status);
     });
 });
 
 http.listen(3000, function(){
     console.log('Listening on HTTP port: 3000');
-});   
+});
 
-//ABOVE = UI CONFIG 
-//BELOW = TCP CONNECTION TO QUADCOPTER 
+//ABOVE = UI CONFIG
+//BELOW = TCP CONNECTION TO QUADCOPTER
 
-//TCP CONFIG 
+//TCP CONFIG
 var net = require('net');
 var HOST = '';
 var MANIFEST_PORT = 5000;
-var DATA_PORT = 6000; 
+var DATA_PORT = 6000;
 
 var ind_buff_size = 128;
- 
+
 //MANNIFEST TCP CONNECTION SETUP
 var manifest_server = net.createServer();
 manifest_server.listen(MANIFEST_PORT, HOST);
@@ -66,63 +66,46 @@ var data_server = net.createServer();
 data_server.listen(DATA_PORT, HOST);
 console.log('listening on Data port: '+ DATA_PORT)
 
-//MANIFEST TABLE 
+//MANIFEST TABLE
 var manifest_arr = [];
 
 function handle_manifest_data(data)
 {
-    var byte_len = data.byteLength
-        
-    //indexing into buffer (vaughn will throw up, need to change these in future)
-    var max_str_len = 20; 
-        
-    console.log("Total buffer size: " + byte_len);
-    for(i = 0; i < byte_len; i += ind_buff_size)	
-    {	
-        var channel_index = data.readUInt32LE(i)  
-        var channel_type = data.readUInt32LE(i+buffer_offset)
-        var channel_size = data.readUInt32LE(i+(2*buffer_offset))
-        var channel_string = data.toString('utf8',i+(3*buffer_offset),i+(3*buffer_offset)+max_str_len);
-        var split = channel_string.split(',');
-        var channel_name = split[0]; 
-        var channel_units = split[1];
-        var channel_object = { 
-            "index"  	:  channel_index, 
-            "data_type"	:  channel_type, 
-            "size"      :  channel_size,
-            "name"      :  channel_name,
-            "units"     :  channel_units
-        }
-        console.log("got channel for manifest:");
-        console.log(channel_object);
-        manifest_arr.push(channel_object)
-    }
-    print_array(manifest_arr);
-    io_3000.emit('manifestLine', manifest_arr); 
+    manifest_lines = data.toString("utf-8").replace("\r\n", "").split(",");
+    var channel_object = {
+        "index"     :  manifest_lines[0],
+        "name"      :  manifest_lines[1],
+        "units"     :  manifest_lines[2],
+        "data_type" :  manifest_lines[3],
+        "size"      :  manifest_lines[4]
+    };
+    console.log(channel_object);
+    manifest_arr.push(channel_object)
+    io_3000.emit('manifestLine', manifest_arr);
 }
 
-//GET INCOMING MANIFESET DATA 
+//GET INCOMING MANIFESET DATA
 manifest_server.on('connection', function(sock)
 {
     console.log('CONNECTED to TCP on port 5000');
     sock.on('data', handle_manifest_data);
-	sock.on('close', (data) => {
-		console.log('TCP connection closed with Manifest Server');	
-	});
+    sock.on('close', (data) => {
+        console.log('TCP connection closed with Manifest Server');
+    });
 });
 
 function increment_byte_index(curr_byte,max_byte)
 {
     //get next chunk of data (32 bits, 4 bytes)
-	if (curr_byte < max_byte)
-		curr_byte += 4;
-	return curr_byte;
+	  if (curr_byte < max_byte)
+		    curr_byte += 4;
+	  return curr_byte;
 }
 
 function handle_telemetry(data)
 {
     console.log("Size of the incoming buffer: " + data.length)
-    var byte_index = 0; 
+    var byte_index = 0;
     var max_byte = data.length;
     var channel_count = data.readUInt32LE(byte_index);
     byte_index = increment_byte_index(byte_index,max_byte);
@@ -132,8 +115,8 @@ function handle_telemetry(data)
     byte_index = increment_byte_index(byte_index,max_byte);
     var channel_index_arr = []
     var data_arr = []
-    
-    //LOOP ONE TO CREATE CHANNEL INDEX ARRAY 
+
+    //LOOP ONE TO CREATE CHANNEL INDEX ARRAY
     for(i = 0; i<channel_count; i++) {
         var index = data.readUInt32LE(byte_index);
         channel_index_arr.push(index)
@@ -153,33 +136,33 @@ function handle_telemetry(data)
         console.log(data_to_emit)
         io_3000.emit(emit_line,data_to_emit)
     }
-    //Updates Green light 
-    var data_status = 1 
-    io_3000.emit("data_status", data_status); 	
+    //Updates Green light
+    var data_status = 1
+    io_3000.emit("data_status", data_status);
 }
 
 function handle_connection_close(data)
 {
-    data_status = 0 
-    io_3000.emit("data_status", data_status); 
-    console.log('TCP connection closed with Manifest Server');	
+    data_status = 0;
+    io_3000.emit("data_status", data_status);
+    console.log('TCP connection closed with Manifest Server');
 }
 
 data_server.on('connection', function(sock) {
     console.log('CONNECTED to TCP via port 6000');
     sock.on('data', handle_telemetry);
-	sock.on('close', handle_connection_close);
+	  sock.on('close', handle_connection_close);
 });
 
 function print_array(arr)
 {
-	for (i = 0; i<arr.length;i++)
+    for (i = 0; i<arr.length;i++)
     {
-		console.log("-------------------------------------------")
-		console.log("Channel Index: " + arr[i].index)
-		console.log("Channel Type: " + arr[i].data_type)
-		console.log("Channel Size: " + arr[i].size)
-		console.log("Channel Name: " + arr[i].name)
-		console.log("Channel Units: " + arr[i].units)
-	}
+        console.log("-------------------------------------------");
+        console.log("Channel Index: " + arr[i].index);
+        console.log("Channel Type: " + arr[i].data_type);
+        console.log("Channel Size: " + arr[i].size);
+        console.log("Channel Name: " + arr[i].name);
+        console.log("Channel Units: " + arr[i].units);
+	  }
 }
