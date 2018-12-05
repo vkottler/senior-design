ceeb5840e9f824e7dbdf7ef86c84d809e2fd697c
Cooper Green <csgreen3@wisc.edu>
2018-12-05 04:32:56 -0600

    Started Control loop

--- src/common/handlers.c
+++ src/common/handlers.c
@@ -4,6 +4,7 @@
 #include "gyro.h"
 #include "battery.h"
 #include "lidar.h"
+#include "esc.h"
 #include "board.h"
 #include "board_config.h"
 #include "i2c.h"
@@ -16,6 +17,140 @@
 
 PC_Buffer *gyro_tx_buf[1], *gyro_rx_buf[1];
 void (*fun_ptr)();
+
+typedef struct {
+	float pid_output_x;
+	float pid_output_y;
+	float pid_output_z;
+	
+	// Difference
+	float curr_dif_x;
+	float curr_dif_y;
+	float curr_dif_z;
+
+	// Proportional
+	float p_gain;
+
+	// Integral
+	float i_gain;
+	float i_output_x;
+	float i_output_y;
+	float i_output_z;
+
+	// Derivative
+	float d_gain;
+	float prev_dif_x;
+	float prev_dif_y;
+	float prev_dif_z;
+
+} control_t;
+
+control_t control;
+
+void control_config()
+{
+	control.pid_output_x = 0;
+	control.pid_output_y = 0;
+	control.pid_output_z = 0;
+	
+	// Difference
+	control.curr_dif_x = 0;
+	control.curr_dif_y = 0;
+	control.curr_dif_z = 0;
+
+	// Proportional
+	control.p_gain = 1;
+
+	// Integral
+	control.i_gain = 1;
+	control.i_output_x = 0;
+	control.i_output_y = 0;
+	control.i_output_z = 0;
+
+	// Derivative
+	control.d_gain = 1;
+	control.prev_dif_x = 0;
+	control.prev_dif_y = 0;
+	control.prev_dif_z = 0;
+}
+
+void calc_pid()
+{	
+	float p_x, i_x, d_x;
+	float p_y, i_y, d_y;
+	float p_z, i_z, d_z;
+	
+    p_x = (control.curr_dif_x) * control.p_gain;
+    i_x = control.i_output_x + ((control.curr_dif_x) * control.i_gain);
+	control.i_output_x = i_x;
+	d_x = (control.curr_dif_x - control.prev_dif_x) * control.d_gain;
+	
+    p_y = (control.curr_dif_y) * control.p_gain;
+    i_y = control.i_output_y + ((control.curr_dif_y) * control.i_gain);
+	control.i_output_y = i_y;
+	d_y = (control.curr_dif_y - control.prev_dif_y) * control.d_gain;
+	
+    p_z = (control.curr_dif_z) * control.p_gain;
+    i_z = control.i_output_z + ((control.curr_dif_z) * control.i_gain);
+	control.i_output_z = i_z;
+	d_z = (control.curr_dif_z - control.prev_dif_z) * control.d_gain;
+
+    control.pid_output_x = p_x + i_x + d_x;
+    control.pid_output_y = p_y + i_y + d_y;
+    control.pid_output_z = p_z + i_z + d_z;
+}
+
+/*****************************************************************************/
+/*         		Y + (CCW)                                                     */
+/*         		|                                                            */
+/*         		|                                                            */
+/*  (CW) -X ____|____ +x (CW)         |Y+                                    */
+/*         		|                     |                               		 */
+/*         		|                  Z+ o___X+                                 */
+/*         		|                                                            */
+/*         		-Y (CCW)                                                      */
+/*****************************************************************************/
+
+void control_loop
+(
+	float raw_input_x, float raw_input_y, float raw_input_z, 
+	float desired_x, float desired_y, float desired_z,
+	float throttle
+) 
+{
+	float esc_pos_x_val;
+	float esc_neg_x_val;
+	float esc_pos_y_val;
+	float esc_neg_y_val;
+
+	// Calculate difference Roll, Pitch and Yaw
+	control.curr_dif_x = raw_input_x - desired_x;
+	control.curr_dif_y = raw_input_y - desired_y;
+	control.curr_dif_z = raw_input_z - desired_z;
+
+	// PID tunning
+	calc_pid();
+
+	// Calculate pulse
+	esc_pos_x_val = throttle + control.pid_output_x 
+					       	 - control.pid_output_z;
+
+	esc_neg_x_val = throttle - control.pid_output_x 
+					       	 - control.pid_output_z;
+
+	esc_pos_y_val = throttle + control.pid_output_y
+					       	 + control.pid_output_z;
+
+	esc_neg_y_val = throttle - control.pid_output_y
+					       	 + control.pid_output_z;
+
+	// Write Motors
+	esc_set_pulse(ESC_TABLE[0], esc_pos_x_val);
+	esc_set_pulse(ESC_TABLE[1], esc_neg_x_val);
+	esc_set_pulse(ESC_TABLE[2], esc_pos_y_val);
+	esc_set_pulse(ESC_TABLE[3], esc_neg_y_val);
+
+}
 
 void fault_handler(unsigned int blink_int) {
 
